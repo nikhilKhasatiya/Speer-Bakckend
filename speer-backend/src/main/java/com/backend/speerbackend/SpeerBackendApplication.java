be2836d45dfe98c095c9ce54f19cc5a289b7812b
nikhil-khasatiya <nikhil.khasatiya@anblicks.com>
2024-01-04 13:06:26 +0530

    Initial commit

---
+++ speer-backend/src/main/java/com/backend/speerbackend/SpeerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.speerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeerBackendApplication.class, args);
+	}
+
+}
